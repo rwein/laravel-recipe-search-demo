d2fab0cb7acc70b828b8f98cb8a7db7d52f3d464
Rob Weinhandl <robweinhandl@gmail.com>
2024-11-02 16:01:50 -0700

    Remove duplicates from ingredient list

--- database/factories/IngredientFactory.php
+++ database/factories/IngredientFactory.php
@@ -10,80 +10,42 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class IngredientFactory extends Factory
 {
     // A generated list of available ingredients
-    public array $ingredients = [
-        // Vegetables & Fruits
-        'Salt', 'Pepper', 'Olive Oil', 'Garlic', 'Onion', 'Butter', 'Flour', 'Sugar',
-        'Milk', 'Eggs', 'Chicken', 'Beef', 'Tomato', 'Carrot', 'Basil', 'Thyme',
-        'Rosemary', 'Oregano', 'Paprika', 'Cumin', 'Cinnamon', 'Vanilla', 'Honey',
-        'Chili Powder', 'Ginger', 'Soy Sauce', 'Vinegar', 'Lemon', 'Cheese', 'Rice',
-        'Pasta', 'Potato', 'Mushroom', 'Fish', 'Shrimp', 'Lettuce', 'Spinach', 'Cucumber',
-        'Bell Pepper', 'Celery', 'Chili Pepper', 'Baking Soda', 'Yeast', 'Coriander',
-        'Parsley', 'Mint', 'Nutmeg', 'Cloves', 'Cardamom', 'Lime', 'Coconut Milk',
-
-        // Additional Vegetables
-        'Broccoli', 'Cauliflower', 'Zucchini', 'Eggplant', 'Sweet Potato', 'Asparagus',
-        'Green Beans', 'Peas', 'Corn', 'Radish', 'Beetroot', 'Brussels Sprouts', 'Kale',
-        'Leek', 'Squash', 'Pumpkin', 'Avocado', 'Artichoke', 'Turnip', 'Parsnip',
-
-        // Fruits
-        'Apple', 'Banana', 'Orange', 'Strawberry', 'Blueberry', 'Raspberry', 'Grapes',
-        'Pineapple', 'Mango', 'Peach', 'Pear', 'Plum', 'Cherry', 'Pomegranate', 'Kiwi',
-        'Watermelon', 'Cantaloupe', 'Papaya', 'Fig', 'Guava', 'Lychee', 'Dragonfruit',
-        'Passionfruit', 'Cranberry', 'Blackberry', 'Clementine', 'Tangerine', 'Apricot',
-
-        // Dairy & Eggs
-        'Cream', 'Sour Cream', 'Yogurt', 'Mozzarella', 'Cheddar', 'Parmesan', 'Feta',
-        'Ricotta', 'Cottage Cheese', 'Gouda', 'Brie', 'Goat Cheese', 'Cream Cheese',
-        'Butter', 'Buttermilk', 'Egg Yolk', 'Egg White', 'Whipping Cream', 'Milk Powder',
-
-        // Meats & Seafood
-        'Pork', 'Bacon', 'Ham', 'Sausage', 'Lamb', 'Duck', 'Turkey', 'Salmon', 'Tuna',
-        'Trout', 'Cod', 'Haddock', 'Crab', 'Lobster', 'Clams', 'Oysters', 'Mussels',
-        'Scallops', 'Octopus', 'Squid', 'Anchovy', 'Sardines', 'Venison', 'Quail',
-
-        // Grains, Pasta, & Legumes
-        'Brown Rice', 'White Rice', 'Couscous', 'Quinoa', 'Barley', 'Oats', 'Farro',
-        'Bulgar', 'Millet', 'Polenta', 'Arborio Rice', 'Orzo', 'Spaghetti', 'Fusilli',
-        'Penne', 'Macaroni', 'Lasagna Noodles', 'Lentils', 'Chickpeas', 'Black Beans',
-        'Kidney Beans', 'Pinto Beans', 'Navy Beans', 'Adzuki Beans', 'Edamame',
-
-        // Spices & Seasonings
-        'Bay Leaf', 'Tarragon', 'Saffron', 'Turmeric', 'Cayenne Pepper', 'Black Pepper',
-        'White Pepper', 'Mustard Seeds', 'Fennel Seeds', 'Fenugreek', 'Anise', 'Allspice',
-        'Mace', 'Star Anise', 'Sumac', 'Za’atar', 'Garam Masala', 'Curry Powder', 'Cajun Spice',
-        'Chinese Five Spice', 'Chili Flakes', 'Chipotle Powder', 'Smoked Paprika', 'Ground Cardamom',
-
-        // Oils, Fats & Vinegars
-        'Sesame Oil', 'Peanut Oil', 'Coconut Oil', 'Vegetable Oil', 'Sunflower Oil',
-        'Canola Oil', 'Ghee', 'Lard', 'Duck Fat', 'Balsamic Vinegar', 'Red Wine Vinegar',
-        'White Wine Vinegar', 'Apple Cider Vinegar', 'Rice Vinegar', 'Sherry Vinegar',
-
-        // Baking & Sweeteners
-        'Brown Sugar', 'Powdered Sugar', 'Maple Syrup', 'Corn Syrup', 'Molasses',
-        'Agave Nectar', 'Stevia', 'Cane Sugar', 'Coconut Sugar', 'Almond Flour', 'Coconut Flour',
-        'Cornmeal', 'Cornstarch', 'Baking Powder', 'Cream of Tartar', 'Gelatin', 'Pectin',
-        'Cocoa Powder', 'Dark Chocolate', 'White Chocolate', 'Chocolate Chips',
-
-        // Nuts & Seeds
-        'Almonds', 'Walnuts', 'Pecans', 'Hazelnuts', 'Cashews', 'Brazil Nuts', 'Macadamia Nuts',
-        'Pistachios', 'Pine Nuts', 'Sunflower Seeds', 'Pumpkin Seeds', 'Chia Seeds', 'Flaxseeds',
-        'Sesame Seeds', 'Hemp Seeds', 'Poppy Seeds',
-
-        // Herbs
-        'Sage', 'Dill', 'Chervil', 'Marjoram', 'Cilantro', 'Lemongrass', 'Lavender',
-        'Bay Leaves', 'Chives', 'Sorrel', 'Tarragon',
-
-        // Condiments & Sauces
-        'Ketchup', 'Mustard', 'Mayonnaise', 'Barbecue Sauce', 'Hot Sauce', 'Worcestershire Sauce',
-        'Sriracha', 'Tabasco', 'Tartar Sauce', 'Hoisin Sauce', 'Oyster Sauce', 'Fish Sauce',
-        'Tahini', 'Peanut Butter', 'Pesto', 'Hummus', 'Guacamole', 'Teriyaki Sauce',
-
-        // Miscellaneous
-        'Breadcrumbs', 'Capers', 'Olives', 'Pickles', 'Sun-Dried Tomatoes', 'Tomato Paste',
-        'Tomato Sauce', 'Stock Cubes', 'Bouillon', 'Miso Paste', 'Salsa', 'Relish',
-        'Dijon Mustard', 'Horseradish', 'Soy Milk', 'Almond Milk', 'Coconut Water', 'Sparkling Water',
-        'Cranberry Juice', 'Apple Juice', 'Orange Juice',
-    ];
+    public static array $ingredients = ['Adzuki Beans', 'Agave Nectar', 'Allspice', 'Almond Flour', 'Almond Milk',
+        'Almonds', 'Anchovy', 'Anise', 'Apple', 'Apple Cider Vinegar', 'Apple Juice', 'Apricot', 'Arborio Rice',
+        'Artichoke', 'Asparagus', 'Avocado', 'Bacon', 'Baking Powder', 'Baking Soda', 'Balsamic Vinegar', 'Banana',
+        'Barbecue Sauce', 'Barley', 'Basil', 'Bay Leaf', 'Bay Leaves', 'Beef', 'Beetroot', 'Bell Pepper',
+        'Black Beans', 'Black Pepper', 'Blackberry', 'Blueberry', 'Bouillon', 'Brazil Nuts', 'Breadcrumbs', 'Brie',
+        'Broccoli', 'Brown Rice', 'Brown Sugar', 'Brussels Sprouts', 'Bulgar', 'Butter', 'Buttermilk', 'Cajun Spice',
+        'Cane Sugar', 'Canola Oil', 'Cantaloupe', 'Capers', 'Cardamom', 'Carrot', 'Cashews', 'Cauliflower',
+        'Cayenne Pepper', 'Celery', 'Cheddar', 'Cheese', 'Cherry', 'Chervil', 'Chia Seeds', 'Chicken', 'Chickpeas',
+        'Chili Flakes', 'Chili Pepper', 'Chili Powder', 'Chinese Five Spice', 'Chipotle Powder', 'Chives',
+        'Chocolate Chips', 'Cilantro', 'Cinnamon', 'Clams', 'Clementine', 'Cloves', 'Cocoa Powder', 'Coconut Flour',
+        'Coconut Milk', 'Coconut Oil', 'Coconut Sugar', 'Coconut Water', 'Cod', 'Coriander', 'Corn', 'Corn Syrup',
+        'Cornmeal', 'Cornstarch', 'Cottage Cheese', 'Couscous', 'Crab', 'Cranberry', 'Cranberry Juice', 'Cream',
+        'Cream Cheese', 'Cream of Tartar', 'Cucumber', 'Cumin', 'Curry Powder', 'Dark Chocolate', 'Dijon Mustard',
+        'Dill', 'Dragonfruit', 'Duck', 'Duck Fat', 'Edamame', 'Egg White', 'Egg Yolk', 'Eggplant', 'Eggs', 'Farro',
+        'Fennel Seeds', 'Fenugreek', 'Feta', 'Fig', 'Fish', 'Fish Sauce', 'Flaxseeds', 'Flour', 'Fusilli',
+        'Garam Masala', 'Garlic', 'Gelatin', 'Ghee', 'Ginger', 'Goat Cheese', 'Gouda', 'Grapes', 'Green Beans',
+        'Ground Cardamom', 'Guacamole', 'Guava', 'Haddock', 'Ham', 'Hazelnuts', 'Hemp Seeds', 'Hoisin Sauce',
+        'Honey', 'Horseradish', 'Hot Sauce', 'Hummus', 'Kale', 'Ketchup', 'Kidney Beans', 'Kiwi', 'Lamb', 'Lard',
+        'Lasagna Noodles', 'Lavender', 'Leek', 'Lemon', 'Lemongrass', 'Lentils', 'Lettuce', 'Lime', 'Lobster',
+        'Lychee', 'Macadamia Nuts', 'Macaroni', 'Mace', 'Mango', 'Maple Syrup', 'Marjoram', 'Mayonnaise', 'Milk',
+        'Milk Powder', 'Millet', 'Mint', 'Miso Paste', 'Molasses', 'Mozzarella', 'Mushroom', 'Mussels', 'Mustard',
+        'Mustard Seeds', 'Navy Beans', 'Nutmeg', 'Oats', 'Octopus', 'Olive Oil', 'Olives', 'Onion', 'Orange',
+        'Orange Juice', 'Oregano', 'Orzo', 'Oyster Sauce', 'Oysters', 'Papaya', 'Paprika', 'Parmesan', 'Parsley',
+        'Parsnip', 'Passionfruit', 'Pasta', 'Peach', 'Peanut Butter', 'Peanut Oil', 'Pear', 'Peas', 'Pecans',
+        'Pectin', 'Penne', 'Pepper', 'Pesto', 'Pickles', 'Pine Nuts', 'Pineapple', 'Pinto Beans', 'Pistachios',
+        'Plum', 'Polenta', 'Pomegranate', 'Poppy Seeds', 'Pork', 'Potato', 'Powdered Sugar', 'Pumpkin',
+        'Pumpkin Seeds', 'Quail', 'Quinoa', 'Radish', 'Raspberry', 'Red Wine Vinegar', 'Relish', 'Rice',
+        'Rice Vinegar', 'Ricotta', 'Rosemary', 'Saffron', 'Sage', 'Salmon', 'Salsa', 'Salt', 'Sardines', 'Sausage',
+        'Scallops', 'Sesame Oil', 'Sesame Seeds', 'Sherry Vinegar', 'Shrimp', 'Smoked Paprika', 'Sorrel', 'Sour Cream',
+        'Soy Milk', 'Soy Sauce', 'Spaghetti', 'Sparkling Water', 'Spinach', 'Squash', 'Squid', 'Sriracha',
+        'Star Anise', 'Stevia', 'Stock Cubes', 'Strawberry', 'Sugar', 'Sumac', 'Sun-Dried Tomatoes', 'Sunflower Oil',
+        'Sunflower Seeds', 'Sweet Potato', 'Tabasco', 'Tahini', 'Tangerine', 'Tarragon', 'Tartar Sauce',
+        'Teriyaki Sauce', 'Thyme', 'Tomato', 'Tomato Paste', 'Tomato Sauce', 'Trout', 'Tuna', 'Turkey', 'Turmeric',
+        'Turnip', 'Vanilla', 'Vegetable Oil', 'Venison', 'Vinegar', 'Walnuts', 'Watermelon', 'Whipping Cream',
+        'White Chocolate', 'White Pepper', 'White Rice', 'White Wine Vinegar', 'Worcestershire Sauce', 'Yeast',
+        'Yogurt', 'Za’atar', 'Zucchini'];
 
     /**
      * Define the model's default state.
@@ -93,7 +55,7 @@ class IngredientFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->unique()->randomElement($this->ingredients),
+            'name' => $this->faker->unique()->randomElement(self::$ingredients),
         ];
     }
 }
