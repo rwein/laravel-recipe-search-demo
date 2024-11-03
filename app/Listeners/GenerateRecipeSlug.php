5031743b539b02b8b0a50d4d7ab8c630f36a28cb
Rob Weinhandl <robweinhandl@gmail.com>
2024-11-03 12:04:29 -0800

    Use guard clause when checking if we need to set a slug
    Very minor, but I tend to like guard clauses where I can use them. Just a matter of personal taste.

--- app/Listeners/GenerateRecipeSlug.php
+++ app/Listeners/GenerateRecipeSlug.php
@@ -22,8 +22,12 @@ class GenerateRecipeSlug
         // Only set a slug if one has not been manually set by whomever is attempting to create a recipe. This is an
         // "escape hatch" of sorts -- there could be situation in the future in which an author/admin would want to
         // opt out of automatic slug creation.
-        if ($event->recipe->slug === null) {
-            $event->recipe->slug = $this->recipeSlugService->generate($event->recipe);
+        if ($event->recipe->slug !== null) {
+            return;
         }
+
+        // Passed by reference so we can just set it here -- this is a hook that runs *before* the model is originally
+        // inserted into the DB
+        $event->recipe->slug = $this->recipeSlugService->generate($event->recipe);
     }
 }
