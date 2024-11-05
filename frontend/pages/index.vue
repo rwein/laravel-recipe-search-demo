0f9751a25e572f5e0c4d545d1ada4d3baf212ebc
Rob Weinhandl <robweinhandl@gmail.com>
2024-11-04 18:20:15 -0800

    Redirect to recipes index when loading root

---
+++ frontend/pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+  import { navigateTo } from 'nuxt/app'
+  import { onMounted } from 'vue'
+
+  onMounted(() => {
+    navigateTo('/recipes') // Redirect to recipes whenever the base path is loaded
+  })
+</script>
