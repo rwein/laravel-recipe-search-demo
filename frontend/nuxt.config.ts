68b3dc61b033268ccf6aa1a1eddd20370875584d
Rob Weinhandl <robweinhandl@gmail.com>
2024-11-04 18:18:16 -0800

    Add api url to nuxt config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  compatibilityDate: "2024-11-03"
+  compatibilityDate: "2024-11-03",
+  runtimeConfig: {
+    public: {
+      apiUrl: process.env.API_URL || 'http://localhost:8888/api'
+    }
+  },
 })
